93af43398137e5b4c6202f8e3bddf8e01b7f2470
aoeni <@example.com>
2024-12-27 15:59:06 +0700

    Initial commit

---
+++ beer-master/app/src/main/java/com/example/beer/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.beer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val findBeer= findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener {
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem
+            val brands = findViewById<TextView>(R.id.brands)
+            brands.text = "Цвет пива - $color"
+        }
+        }
+}
